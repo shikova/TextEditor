e713901b71b04b37138ac7a19a5b3f7f5525fb6a
shikova <myewalid@gmail.com>
2017-01-10 23:52:28 +0000

    add buttons and function to  compile java file

--- src/com/shikova/Constants.java
+++ src/com/shikova/Constants.java
@@ -4,9 +4,14 @@ package com.shikova;
  * Created by walid on 1/2/2017.
  */
 public class Constants {
-    public static final String STAT_FILE_NOT_SAVED="Unsaved File";
-    public static final String STAT_FILE_SAVED="Ready";
-    public static final String APP_NAME="Text Editor";
-    public static final String ABOUT_TEXT="Created by Lee Hwang Chee Team ©";
-    public static final String GITHUB_REPO_URL="https://github.com/shikova/com.shikova.TextEditor";
+    public class str {
+        public static final String STAT_FILE_NOT_SAVED = "Unsaved File";
+        public static final String STAT_FILE_SAVED = "Ready";
+        public static final String APP_NAME = "Text Editor";
+        public static final String ABOUT_TEXT = "Created by Lee Hwang Chee Team :D ©\nWalid Elalaouy\nAbdelati Elasri\nAnas Elkamali ";
+        public static final String GITHUB_REPO_URL = "https://github.com/shikova/com.shikova.TextEditor";
+    }
+    public class icon{
+
+    }
 }
